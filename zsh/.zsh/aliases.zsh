8f007a5a93f1c3f88d562445bb5e690b746fd24d
Stefan Novak <stefan@novak.io>
2016-05-11 11:00:06 -0700

    update aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -5,6 +5,13 @@ alias la='gls -A --color'
 alias ll="gls -l --color"
 alias ls="gls -F --color"
 alias lt='gls -lArht --color'
-alias priceiswrong='afplay ~/.sounds/priceiswrong.mp3'
 alias restartpow="touch ~/.pow/restart.txt"
+
+# sounds
+alias priceiswrong='afplay ~/.sounds/priceiswrong.mp3'
 alias rimshot='afplay ~/.sounds/rimshot.mp3'
+
+# git
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative --all"
+alias gp='git push origin HEAD'
+alias gs='git status -sb'
